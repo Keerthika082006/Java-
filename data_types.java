232a5d6e709e74a39b58196ce49f5957aa74fa2f
Keerthika <dkeerthika1808@gmail.com>
2025-09-22 21:24:01 +0530

    second commit
    Data types in Java

---
+++ data_types.java
@@ -0,0 +1,36 @@
+class data_types{
+    public static void main(String[] args) {
+        byte a=5;
+        System.out.println(a);
+
+        short b=336;
+        System.out.println(b);
+
+        int c=-4;
+        System.out.println(c);
+
+        long d=1234567890;
+        System.out.println(d);
+
+        float e=21.223456789f;//only 6 decimal will print
+        System.out.println(e);
+
+        double f=34.1234567;
+        System.out.println(f);
+
+        char i = 'k';
+        System.out.println(i);
+
+        String name= "Keerthi";
+        System.out.println(name.charAt(5));
+
+        boolean o = true;
+        boolean p = false;
+        System.out.println(o);
+        System.out.println(p);
+
+
+
+
+    }
+}
