82f2ba354e7155b1e5893a504afd87cae02d6e37
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-22 21:15:03 +0530

    Second commit

---
+++ variable.java
@@ -0,0 +1,20 @@
+/* class variable{
+    public static void main(String[] arg){
+        int container = 23;
+        String keerthi = "K";
+        String Container = keerthi;
+        System.out.println(container);
+        System.out.println(Container);
+
+    }     
+} */
+//adding numbers
+class variable{
+    public static void main(String[] args){
+        int a = 4;
+        int b = 2;
+        int c = 4;
+        int d = a+b+c;
+        System.out.println(d);
+    }
+} 
