6c3e4bb213ff7015f98f548604b340a2572046ba
Keerthika <dkeerthika1808@gmail.com>
2025-09-23 23:41:53 +0530

    first commit
    How to get input from the user

---
+++ inputOutput.java
@@ -0,0 +1,11 @@
+  import java.util.Scanner;
+public class inputOutput {
+ 
+    public void printNumber(Scanner sc) {
+        Scanner ob = new Scanner(System.in);
+        int x = ob.nextInt();
+        System.out.print(x);
+
+    }
+}  
+
